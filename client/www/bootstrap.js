beb06dabf53131f3579750941095d9cd4db4db7f
Freedom He <furui.he@gaeamobile-inc.net>
2015-01-04 10:33:51 +0800

    requirejs

--- client/www/bootstrap.js
+++ client/www/bootstrap.js
@@ -2,13 +2,25 @@ require.config({
     baseUrl: '/js',
     paths: {
   		'cordova': '/cordova',
+        'purl': '/lib/purl/purl',
+        'restangular': '/lib/restangular/dist/restangular',
+        'lodash': '/lib/lodash/dist/lodash',
     },
 	shim: {
 		'corodva': {
 			exports: 'cordova'
 		},
+        'purl': {
+            exports: 'purl'
+        },
+        'lodash': {
+            exports: '_'
+        },
+        'restangular': {
+            deps: ['lodash']
+        },
 		'app': {
-			deps: ['cordova']
+			deps: ['cordova', 'purl', 'restangular']
 		},
         // 'controllers': {
         //     deps: ['app']
