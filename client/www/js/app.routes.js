8f873dc06847d0a9b19a85a565bd6deac936efa2
Qoli Wong <llqoli@gmail.com>
2015-01-05 19:31:13 +0800

    Fix segmented-control

---
+++ client/www/js/app.routes.js
@@ -0,0 +1,135 @@
+
+define(['app'], function(app){
+
+  app.config(function($stateProvider, $urlRouterProvider) {
+
+
+    var access = app.routingConfig.accessLevels;
+
+    //路由配置
+    $stateProvider
+
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
+    })
+
+    //Should not delete the placeHolder
+    //stateProviderPlaceHolder
+
+        .state('app.games', {
+        url: '/games',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/games.html',
+            controller: 'GamesCtrl'
+          }
+        },
+        data: {
+          access: access.public
+        }
+      })
+
+
+        .state('app.game', {
+        url: '/games/:gameId' ,
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/game.html',
+            controller: 'GameCtrl'
+          }
+        },
+        data: {
+          access: access.public
+        }
+      })
+
+
+
+        .state('app.start', {
+        url: '/start',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/start.html',
+            controller: 'StartCtrl'
+          }
+        },
+        data: {
+          access: access.public
+        }
+      })
+
+
+        .state('app.user-info', {
+        url: '/user_info',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/user-info.html',
+            controller: 'UserInfoCtrl'
+          }
+        },
+        data: {
+          access: access.user
+        }
+      })
+
+
+    .state('app.search', {
+      url: "/search",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/search.html"
+        }
+      },
+      data: {
+        access: access.public
+      }
+    })
+
+    .state('app.browse', {
+      url: "/browse",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/browse.html"
+        }
+      },
+      data: {
+        access: access.user
+      }
+    })
+
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      },
+      data: {
+        access: access.public
+      }
+    })
+
+    .state('app.single', {
+      url: "/playlists/:playlistId",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlist.html",
+          controller: 'PlaylistCtrl'
+        }
+      },
+      data: {
+        access: access.public
+      }
+    });
+    // if none of the above states are matched, use this as the fallback
+    if (localStorage.getItem('user') !== null){
+      $urlRouterProvider.otherwise('/app/games/1');
+    }else{
+      $urlRouterProvider.otherwise('/app/start');
+    }
+  });
+});
